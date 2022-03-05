ab1725199980378c7308c5fae59151dc505a60fa
Александр Макаров <artemi91obo@rambler.ru>
2022-03-05 09:30:58 +0700

    Add New enums (Color, Season, Manufactures, EducationForm, Genre)

---
+++ Programming/Programming/Model/Color.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    public enum Color
+    {
+        White,
+        Black,
+        Red,
+        Green,
+        Blue,
+        Purple,
+        Yellow,
+        Magenta
+    }
+}
