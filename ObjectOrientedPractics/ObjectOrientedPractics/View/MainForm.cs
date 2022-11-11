51a3a38dd29b3e8cebeabb7391421a07f1922f38
Александр Макаров <artemi91obo@rambler.ru>
2022-11-11 17:27:24 +0700

    lab 2 - save

--- ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.cs
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.cs
@@ -26,10 +26,18 @@ namespace ObjectOrientedPractics
         {
             InitializeComponent();
 
-            _store = new Store();
+            _store = ProjectSerializer.Deserialize();
 
             itemsTab1.Items = _store.Items;
             customersTab1.Customers = _store.Customers;
+        }
+
+        private void MainForm_Closing(object sender, FormClosingEventArgs e)
+        {
+            _store.Customers = customersTab1.Customers;
+            _store.Items = itemsTab1.Items;
+
+            ProjectSerializer.Serialize(_store);
         }
     }
 }
