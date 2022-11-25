916f037434f9d0f6d4d30d391cb94b59ea1e7327
Александр Макаров <artemi91obo@rambler.ru>
2022-11-25 19:42:41 +0700

    Revert "Lab 3 - list in cart"
    This reverts commit a4e4218a0b48ec2ae1c799c3a0c5642d3de39433.

--- ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/CartsTab.cs
+++ ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/CartsTab.cs
@@ -7,51 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using ObjectOrientedPractics.Model;
 
 namespace ObjectOrientedPractics.View.Tabs
 {
     public partial class CartsTab : UserControl
     {
-        /// <summary>
-        /// Коллекция товаров.
-        /// </summary>
-        private List<Item> _items;
-
-        /// <summary>
-        /// Коллекция покупателей.
-        /// </summary>
-        private List<Customer> _customers;
-
         public CartsTab()
         {
             InitializeComponent();
-        }
-
-        /// <summary>
-        /// Возвращает и задает коллекцию товаров.
-        /// </summary>
-        public List<Item> Items
-        {
-            get { return _items; }
-            set
-            {
-                _items = value;
-
-            }
-        }
-
-        /// <summary>
-        /// Возвращает и задает коллекцию покупателей.
-        /// </summary>
-        public List<Customer> Customers
-        {
-            get { return _customers; }
-            set
-            {
-                _customers = value;
-
-            }
         }
     }
 }
